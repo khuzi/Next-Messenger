fa4e4cfbe8a2d67daea1ddc20839e164f1438293
khuzaima <khuzaimayaseen@gmail.com>
2020-12-27 02:58:55 +0500

    Add all redux dependencies.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
+
+import { theme } from "../theme";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-export default MyApp
+  return (
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </StylesProvider>
+    </ThemeProvider>
+  );
+}
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
